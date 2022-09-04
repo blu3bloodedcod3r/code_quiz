8e8928cd54bfff471ce0c152a2462d84bc46fd0d
Crystal Aguilar <crystal_0688@yahoo.com>
2022-09-03 17:43:13 -0700

    fixing errors to display issues

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,47 +1,51 @@
-const startButton = document.getElementById('start')
-const nextButton = document.getElementById('next')
-const questionContainer = document.getElementById("question-cont")
-const questionEl = document.getElementById('question')
-const answerButtons = document.getElementById('answ-btn')
+const startButton = document.getElementById('start-button');
+const nextButton = document.getElementById('next-button');
+const questionContainer = document.getElementById("question-cont");
+const questionEl = document.getElementById('question');
+const answerButtons = document.getElementById('answ-btn');
 
 let shuffleQuestions, currentQuestionIndex
 
-startButton.addEventListener('click', startGame)
-
+startButton.addEventListener('click', startGame);
+nextButton.addEventListener('click', ()=> {
+    currentQuestionIndex++;
+    setNextQuestion();
+});
 
 function startGame() {
-    startButton.classList.add('hide')
-    shuffleQuestions = question.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
-    questionContainer.classList.remove('hide')
-    setNextQuestion()
-}
+    startButton.classList.add('hide');
+    shuffleQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
+    questionContainer.classList.remove('hide');
+    setNextQuestion();
+};
 
 function setNextQuestion() {
-    resetState()
-    showQuestion(shuffleQuestions[currentQuestionIndex])
-}
+    resetState();
+    showQuestion(shuffleQuestions[currentQuestionIndex]);
+};
 
 function showQuestion(question) {
-    questionEl.innerText = question.question
+    questionEl.innerText = question.question;
     question.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('btn')
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
         if (answer.correct) {
-            button.sataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtons.appendChild(button)
-    })
-}
+            button.sataset.correct = answer.correct;
+        };
+        button.addEventListener('click', selectAnswer);
+        answerButtons.appendChild(button);
+    });
+};
 
 function resetState() {
-    nextButton.classList.add('hide')
+    clearStatusClass(document.body);
+    nextButton.classList.add('hide');
     while(answerButtons.firstChild) {
-        answerButtons.removeChild(answerButtons.firstChild)
-    }
-}
+        answerButtons.removeChild(answerButtons.firstChild);
+    };
+};
 
 function selectAnswer() {
     const selectedButton = e.target
@@ -50,11 +54,13 @@ function selectAnswer() {
     Array.from(answerButtons.childern).forEach(button => {
         setStatusClass(button, button.dataset.correct)
     })
+    (shuffleQuestions.length > currentQuestionIndex + 1) ? nextButton.classList.remove('hide') : startButton.innerText ="Restart"; startButton.classList.remove
+    nextButton.classList.remove('hide')
 }
 
 function setStatusClass(element, correct) {
     clearStatusClass(element)
-    correct ? element.classList.add('correct') : element.classList.add('wrong')
+    (correct) ? element.classList.add('correct') : element.classList.add('wrong')
 }
 
 function clearStatusClass (element) {
@@ -64,10 +70,31 @@ function clearStatusClass (element) {
 
 const questions = [
     {
-        question: "What is 2+2",
-        answer: [
-            { text: '4', correct: true},
-            {text: "22", correct: false}
-    ]
+        question: "Who invented JavaScript?",
+        answers: {
+            a: "Douglas Crockford",
+            b: "Sheryl Sandberg",
+            c: "Brendan Eich"
+    },
+        correctAnswer: "c"
+  },
+  {
+    question: "Which one of these is a JavaScript package manager?",
+    answers: {
+      a: "Node.js",
+      b: "TypeScript",
+      c: "npm"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Which tool can you use to ensure code quality?",
+    answers: {
+      a: "Angular",
+      b: "jQuery",
+      c: "RequireJS",
+      d: "ESLint"
+    },
+    correctAnswer: "d"
     }
 ]
